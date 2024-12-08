ac1cab5e9644f72882dfde2fa2611925de23e195
jerodev <jeroen@deviaene.eu>
2024-12-08 21:45:56 +0100

    2024.07 Part 2

--- 2024/7-bridge/main.go
+++ 2024/7-bridge/main.go
@@ -4,7 +4,7 @@ import (
 	"advent-of-code/util"
 	"bufio"
 	"fmt"
-	"math"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -20,6 +20,9 @@ var modifiers = []func(int, int) int{
 
 		return i1 * i2
 	},
+	func(i1, i2 int) int {
+		return i1*util.IntPow(10, util.IntLength(i2)) + i2
+	},
 }
 
 func main() {
@@ -27,7 +30,7 @@ func main() {
 	scan := bufio.NewScanner(file)
 
 	var parts []string
-	var result, sum int
+	var result, result2, sum int
 	var numbers []int
 
 	for scan.Scan() {
@@ -35,18 +38,22 @@ func main() {
 		sum, _ = strconv.Atoi(parts[0])
 		numbers = util.StringToInts(parts[1], " ")
 
-		if resolves(sum, numbers) {
+		if resolves(sum, numbers, 2) {
 			result += sum
+			result2 += sum
+		} else if resolves(sum, numbers, 3) {
+			result2 += sum
 		}
 	}
 
 	fmt.Println(result)
+	fmt.Println(result2)
 }
 
-func resolves(sum int, numbers []int) bool {
-	possibilities := int(math.Pow(2, float64(len(numbers)-1)))
+func resolves(sum int, numbers []int, base int) bool {
+	possibilities := util.IntPow(base, len(numbers)-1)
 	for i := range possibilities {
-		bin := ("0000000000" + fmt.Sprintf("%b", i))
+		bin := ("0000000000" + big.NewInt(int64(i)).Text(base))
 		bin = bin[len(bin)-len(numbers)+1:]
 
 		total := numbers[0]
--- util/math.go
+++ util/math.go
@@ -9,3 +9,30 @@ func Abs[I constraints.Integer](v I) I {
 
 	return v
 }
+
+func IntLength(v int) int {
+	x, count := 10, 1
+
+	for x <= v {
+		x *= 10
+		count++
+	}
+
+	return count
+}
+
+func IntPow(n, m int) int {
+	if m == 0 {
+		return 1
+	}
+
+	if m == 1 {
+		return n
+	}
+
+	result := n
+	for i := 2; i <= m; i++ {
+		result *= n
+	}
+	return result
+}
